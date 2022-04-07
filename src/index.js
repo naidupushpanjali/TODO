9efca8a9dea7a7e5af2db08ab4a5e4be7e7e3c1a
Pushpanjali Naidu <pushpanjalinaidu@Pushpanjalis-MacBook-Air.local>
2022-04-07 19:28:08 +0530

    Removed strict mode for react 18 application

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import './styles/index.scss';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
-import * as ReactDOMClient from 'react-dom/client';
+import React from "react";
+import "./styles/index.scss";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+import * as ReactDOMClient from "react-dom/client";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
 
-root.render(
-    <App />
-);
+root.render(<App />);
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measurinDg performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
